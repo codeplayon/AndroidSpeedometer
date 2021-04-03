b21ed9d5cb2db624327fbb4b12013c89c70b5a7c
codeplayon <codeplayon@gmail.com>
2021-04-03 08:10:40 -0400

    How to make Speedometer in Android

--- app/src/main/java/com/codeplayon/speedometer/MainActivity.java
+++ app/src/main/java/com/codeplayon/speedometer/MainActivity.java
@@ -2,13 +2,34 @@ package com.codeplayon.speedometer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+SpeedometerView Speed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Speed = (SpeedometerView)findViewById(R.id.speedometer);
+        Speed.setLabelConverter(new SpeedometerView.LabelConverter() {
+            @Override
+            public String getLabelFor(double progress, double maxProgress) {
+                return String.valueOf((int) Math.round(progress));
+            }
+        });
+
+// configure value range and ticks
+        Speed.setMaxSpeed(100);
+        Speed.setMajorTickStep(25);
+        Speed.setMinorTicks(0);
+
+// Configure value range colors
+        Speed.addColoredRange(0, 50, Color.GREEN);
+        Speed.addColoredRange(50, 75, Color.YELLOW);
+        Speed.addColoredRange(75, 100, Color.RED);
+        Speed.setSpeed(25, 2000, 500);
     }
 }
